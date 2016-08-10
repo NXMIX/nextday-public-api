17c0e44d08869a3157a937d4cb8df371318c1804
jacobbubu <rong.shen@gmail.com>
2016-08-10 15:37:42 +0800

    Put `path` and `query` string in to a single variable

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ -n "$1" ] && [ -n "$2" ]; then
-    endpoint='http://api.nextday.im/api/calendar?from=20141001&to=20141003'
-    url='/api/calendar?from=20141001&to=20141003'
+    pathAndQuery="/api/calendar?from=20141001&to=20141003"
+    endpoint="http://api.nextday.im$pathAndQuery"
     name=$1
     secret=$2
     date=$(date +'%Y-%m-%d %H:%M:%S')
-    echo -n "$url&$name&$date&$secret" | md5sum | cut -d" " -f1 | { read hash; curl -H "Date: $date" -H "Authorization: $name:$hash" $endpoint; }
+    echo -n "$pathAndQuery&$name&$date&$secret" | md5sum | cut -d" " -f1 | { read hash; curl -H "Date: $date" -H "Authorization: $name:$hash" $endpoint; }
 else
     echo "[name] and [secret] are required." >&2
     echo "e.g. './test.sh [name] [secret]'" >&2
--- testMac.sh
+++ testMac.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ -n "$1" ] && [ -n "$2" ]; then
-    endpoint='http://api.nextday.im/api/calendar?from=20141001&to=20141003'
-    url='/api/calendar?from=20141001&to=20141003'
+    pathAndQuery="/api/calendar?from=20141001&to=20141003"
+    endpoint="http://api.nextday.im$pathAndQuery"
     name=$1
     secret=$2
     date=$(date +'%Y-%m-%d %H:%M:%S')
-    hash=$(md5 -q -s "$url&$name&$date&$secret")
+    hash=$(md5 -q -s "$pathAndQuery&$name&$date&$secret")
     curl -H "Date: $date" -H "Authorization: $name:$hash" $endpoint
 else
     echo "[name] and [secret] are required." >&2
