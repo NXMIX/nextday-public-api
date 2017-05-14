e2e1369cbf3ffe71239d59962aa53df42686949d
jacobbubu <rong.shen@gmail.com>
2017-05-14 16:36:07 +0800

    修复 #2, 使用 `X-ND-Date` Header 替换掉原来的 `Date` Header
    原有 `Date` 仍可继续使用。

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ if [ -n "$1" ] && [ -n "$2" ]; then
     name=$1
     secret=$2
     date=$(date +'%Y-%m-%d %H:%M:%S')
-    echo -n "$pathAndQuery&$name&$date&$secret" | md5sum | cut -d" " -f1 | { read hash; curl -H "Date: $date" -H "Authorization: $name:$hash" $endpoint; }
+    echo -n "$pathAndQuery&$name&$date&$secret" | md5sum | cut -d" " -f1 | { read hash; curl -H "X-ND-Date: $date" -H "Authorization: $name:$hash" $endpoint; }
 else
     echo "[name] and [secret] are required." >&2
     echo "e.g. './test.sh [name] [secret]'" >&2
--- testMac.sh
+++ testMac.sh
@@ -1,12 +1,14 @@
+# ./testMac.sh yourId yourSecret | jq '(.result)| keys'
 #!/bin/sh
 if [ -n "$1" ] && [ -n "$2" ]; then
-    pathAndQuery="/api/calendar?from=20141001&to=20141003"
+    # pathAndQuery="/api/calendar?from=20141001&to=20141003"
+    pathAndQuery="/api/calendar?from=20161230&to=20161230"
     endpoint="http://api.nextday.im$pathAndQuery"
     name=$1
     secret=$2
     date=$(date +'%Y-%m-%d %H:%M:%S')
     hash=$(md5 -q -s "$pathAndQuery&$name&$date&$secret")
-    curl -H "Date: $date" -H "Authorization: $name:$hash" $endpoint
+    curl -H "X-ND-Date: $date" -H "Authorization: $name:$hash" $endpoint
 else
     echo "[name] and [secret] are required." >&2
     echo "e.g. './testMac.sh [name] [secret]'" >&2
