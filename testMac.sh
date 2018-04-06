b21bb50fb499d13e0740ea6a6f5e3b499c7ae1bb
jacobbubu <rong.shen@gmail.com>
2018-04-06 17:03:31 +0800

    Using TLSv1.2 forcedly for https access

--- testMac.sh
+++ testMac.sh
@@ -9,7 +9,7 @@ if [ -n "$1" ] && [ -n "$2" ]; then
     secret=$2
     date=$(date +'%Y-%m-%d %H:%M:%S')
     hash=$(md5 -q -s "$pathAndQuery&$name&$date&$secret")
-    curl -H "X-ND-Date: $date" -H "Authorization: $name:$hash" $endpoint
+    curl --tlsv1.2 -H "X-ND-Date: $date" -H "Authorization: $name:$hash" $endpoint
 else
     echo "[name] and [secret] are required." >&2
     echo "e.g. './testMac.sh [name] [secret]'" >&2
